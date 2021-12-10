cfda47dc211905f0ae09441039ab4cf5429d326a
Mauriciiow <mauricioliveira639@gmail.com>
2021-12-10 00:59:59 -0300

    projeto na metade basicamente

---
+++ script.js
@@ -0,0 +1,27 @@
+function buscarImagem() {
+  let data = $('#data').val()
+  let imagens = $('#imagens')
+  let titulo = $('#titulo-imagem')
+  let texto = $('#texto-imagem')
+  let dataValor = $('#data-valor')
+  $.ajax({
+    url: `https://api.nasa.gov/planetary/apod?api_key=XnAgwqooFbXp4A6DFWFzbjxwI8VfeehnZvtKtCLt&date=${data}`,
+    success: function (response) {
+      imagens.attr('src', response.url)
+      titulo.text(response.title)
+      texto.text(response.explanation)
+      dataValor.text(data)
+      console.log(response);
+      
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  })
+
+
+}
+let botao = $('#botao')
+botao.click(()=>{
+  return buscarImagem()
+}) 
