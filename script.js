b2650394e93506a766ed3b5862f34ae151200f9b
Mauriciiow <mauricioliveira639@gmail.com>
2021-12-13 18:57:10 -0300

    implementei a funcao de aparecer o video e o autor

--- script.js
+++ script.js
@@ -4,15 +4,41 @@ function buscarImagem() {
   let titulo = $('#titulo-imagem')
   let texto = $('#texto-imagem')
   let dataValor = $('#data-valor')
+  let video = $('#video')
+  let cop = $('#copyright')
 
 
   $.ajax({
     url: `https://api.nasa.gov/planetary/apod?api_key=XnAgwqooFbXp4A6DFWFzbjxwI8VfeehnZvtKtCLt&date=${data}`,
-    success: function (response) {
-      imagens.attr('src', response.url)
+    type: 'GET', 
+    contentType: 'application/json',
+     success: function (response) {
       titulo.text(response.title)
       texto.text(response.explanation)
       dataValor.text(data)
+
+      let verificaVideo = function () {
+        if (response.media_type == 'video') {
+          imagens.css('display', 'none')
+          video.css('display', 'flex')
+          video.attr('src', response.url)
+        } else{
+          video.css('display', 'none')
+          imagens.css('display', 'flex')
+          imagens.attr('src', response.url)
+        }
+      }
+      verificaVideo()
+
+     let verificaAutor = function() {
+      if (typeof response.copyright === 'undefined' ) {
+        cop.text(`Image Credit & Copyright: Autor Desconhecido`)
+      } else {
+        cop.text(`Image Credit & Copyright: ${response.copyright}`)
+      }
+     }
+     verificaAutor()
+      
       
     },
     error: function (error) {
@@ -28,3 +54,4 @@ botao.click(()=>{
   rodape.css('display', 'flex')
   return buscarImagem()
 }) 
+
